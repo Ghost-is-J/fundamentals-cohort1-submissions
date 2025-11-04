bf42d9324b2c1a8a8a33e7eba8654d6606820bcc
Afolayan Stephen <afolayan4life@gmail.com>
2025-11-04 08:59:14 +0100

    Delete fundamentals-cohort1-submissions directory

--- fundamentals-cohort1-submissions/Dev-dave/week-4/backend/devconnect-backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUserProfile } from "../controllers/userController.js";
-import { authenticate } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/me", authenticate, getUserProfile);
-
-export default router;
--- fundamentals-cohort1-submissions/Dev-dave/week-5/backend/pulsetrack-backend/routes/doctorRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getDoctors, createDoctor } from "../controllers/doctorController.js";
-
-const router = express.Router();
-
-router.get("/", getDoctors);
-router.post("/", createDoctor);
-
-export default router;
